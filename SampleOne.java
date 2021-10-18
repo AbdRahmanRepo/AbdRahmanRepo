eaea9bf7c1e7f22c9934b8d8ad8644e1e1eb2a73
AbdulRahman <92000559+AbdRahmanRepo@users.noreply.github.com>
2021-10-18 07:26:16 +0530

    Package Line Deleted

--- SampleOne.java
+++ SampleOne.java
@@ -1,7 +1,7 @@
-package com.google.com.GitHubDemo;
+
 
 public class SampleOne {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World from github");
 	}
 }

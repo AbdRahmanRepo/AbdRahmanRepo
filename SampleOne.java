5e9038db176313ab0ed8602e3f88e77c44c98239
AbdulRahman <92000559+AbdRahmanRepo@users.noreply.github.com>
2021-10-19 06:56:36 +0530

    space deleted

--- SampleOne.java
+++ SampleOne.java
@@ -3,6 +3,6 @@
 public class SampleOne {
 	public static void main(String[] args) {
 		System.out.println("Hello World from github");
-		System.out.println("Hello World pushed");
+		System.out.println("Hello Worldpushed");
 	}
 }
